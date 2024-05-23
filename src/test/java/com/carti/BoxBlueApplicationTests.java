90a658dd8cc54efd0edfa0ab518947947115e6d1
Leonardo Sigolo <leonardo.sigolo1037@gmail.com>
2024-05-23 14:26:32 -0300

    BoxBlue

---
+++ src/test/java/com/carti/BoxBlueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxBlueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
